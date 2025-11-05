fd0637c8b9180d1b24f58b91a0c0106f216182f9
Hardik Pratap <hardikpratap188@gmail.com>
2025-11-06 00:25:01 +0530

    done likSchema

---
+++ server/src/models/linkmodel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Types } from "mongoose";
+import { Platform } from "../utils/constant";
+
+export interface ILink{
+    title?: string;
+  url: string;
+  previewImage?: string;
+  description?: string;
+  tags?: string[];
+  type: Platform;
+  metadata?: Record<string, any>;
+  brainId: Types.ObjectId;
+  ownerId: Types.ObjectId;
+  isPublic: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const linkSchema: Schema<ILink>= new Schema({
+    title:{type:String, required:true},
+    url:{type:String, required:true},
+    previewImage:{type:String},
+    description:{type:String},
+    tags:[String],
+    type:{ type: String, enum: ["youtube", "twitter", "facebook", "website", "custom"], default: "website" },
+    metadata: Schema.Types.Mixed,
+    brainId: { type: Schema.Types.ObjectId, ref: "Brain", required: true },
+    ownerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    isPublic: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+);
