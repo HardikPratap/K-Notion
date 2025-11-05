aeb3693f55d86621b80f8fb3d9d01dd3de1227ed
Hardik Pratap <hardikpratap188@gmail.com>
2025-11-05 23:34:51 +0530

    auth middleware done

---
+++ server/src/middleware/authmiddleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { ENV } from "../config/env";
+// import User from "../models/user.model";
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+export async function protect(req: AuthRequest, res: Response, next: NextFunction) {
+  try {
+    const header = req.headers.authorization;
+    if (!header || !header.startsWith("Bearer ")) {
+      return res.status(401).json({ success: false, message: "Not authorized" });
+    }
+    const token = header.split(" ")[1];
+    const decoded: any = jwt.verify(token, ENV.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password").lean();
+    if (!user) return res.status(401).json({ success: false, message: "User not found" });
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ success: false, message: "Not authorized" });
+  }
+}
